6b420fbff252722f8205b8e1b892f7efd0a51ddb
brillianodhiya <brillidhiya@gmail.com>
2019-09-17 01:22:16 +0700

    auth first commit

---
+++ src/models/user.js
@@ -0,0 +1,14 @@
+const conn = require('../config/dbconfig')
+
+module.exports = {
+    findUserByEmail: (email) => new Promise((resolve, reject) => {
+        conn.query('SELECT * FROM user WHERE email = ?', [email], (err, row) => {
+            !err ? resolve(row) : reject(err)
+        })
+    }),
+    createUser: (user) => new Promise((resolve, reject) => {
+        conn.query('INSERT INTO user (first_name, last_name, email, password, birth) VALUES (?,?,?,?,?)', user, (err, result) => {
+            !err ? resolve(row) : reject(err)
+        })
+    })
+}
