e1ac8d8edf589219ce9efe6ff59441abf158e9f8
brillianodhiya <brillidhiya@gmail.com>
2019-09-20 17:40:12 +0700

    fixing redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 const redis = require('redis')
-const url = process.env.REDISCLOUD_URL
-console.log(url)
-const client = redis.createClient({url})
+const url = require('url')
+const redisUrl = url.parse(process.env.REDISCLOUD_URL)
+console.log(redisUrl)
+const client = redis.createClient(redisUrl.port, redisUrl.hostname, {no_ready_check: true})
+client.auth(redisUrl.auth.split(":")[1])
 // const client = url ? redis.createClient({url}) : redis.createClient(6379)
-client.on("error", err => {
-    console.log("Error " + err);
-});
 module.exports=client
