e42c5bc7a21b659f1478bf022cfe37135d721bb1
brillianodhiya <brillidhiya@gmail.com>
2019-09-20 16:43:07 +0700

    reconfig

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const redis = require('redis')
 const url = process.env.REDISCLOUD_URL
 console.log(url)
-const client = redis.createClient({url, port: 6379}) || redis.createClient(6379)
+const client = url ? redis.createClient({url}) : redis.createClient(6379)
 client.on("error", err => {
     console.log("Error " + err);
 });
